69373fc8f443e1f2551172a9a76d33e97705bfca
Trevor L. McDonell <trevor@passivelogic.com>
2023-11-16 13:54:27 +0100

    set .ll files to LLVM

--- init.lua
+++ init.lua
@@ -85,6 +85,14 @@ vim.api.nvim_create_autocmd('OptionSet',
     end
   })
 
+vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'},
+  { desc = 'Set filetype to LLVM'
+  , pattern = '*.ll'
+  , callback = function()
+      vim.opt_local.filetype='llvm'
+    end
+  })
+
 if vim.env.TERM == 'xterm-kitty' then
   vim.api.nvim_create_autocmd({'VimEnter', 'FocusGained'},
     { desc = "Check terminal background colour and update colour scheme"
