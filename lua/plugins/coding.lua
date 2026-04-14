eb2602dfb81b204559c538dddb983e4e2b69df8d
Trevor L. McDonell <trevor@passivelogic.com>
2026-04-14 13:06:58 +0200

    nvim-treesitter is no longer needed (archived)

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,16 +1,4 @@
 return {
-  { 'nvim-treesitter/nvim-treesitter'
-  , build = function()
-      require('nvim-treesitter.install').update({ with_sync = true })
-    end
-  , config = function()
-      require('nvim-treesitter.configs').setup
-        { auto_install = true
-        , highlight = { enable = true }
-        }
-    end
-  },
-
   { 'nvim-telescope/telescope.nvim'
   , dependencies =
       { 'nvim-telescope/telescope-fzf-native.nvim'
