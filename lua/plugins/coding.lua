40424c7b67e29ad8bc4efed62e42ef6b9c1fa7e9
Trevor L. McDonell <trevor@passivelogic.com>
2026-04-20 13:38:15 +0200

    nvim-treesitter is just different now

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,4 +1,18 @@
 return {
+  { 'nvim-treesitter/nvim-treesitter'
+  , branch = 'main'
+  , init = function()
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function()
+          -- Enable treesitter highlighting and disable regex syntax
+          pcall(vim.treesitter.start)
+          -- Enable treesitter-based indentation
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end
+  },
+
   { 'nvim-telescope/telescope.nvim'
   , dependencies =
       { 'nvim-telescope/telescope-fzf-native.nvim'
