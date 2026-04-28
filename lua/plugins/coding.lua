6d446b95ee8f1e459b36cca3d92fe210d370b743
Trevor L. McDonell <trevor@passivelogic.com>
2026-04-28 09:34:25 +0200

    update LSP

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -111,11 +111,11 @@ return {
 
       vim.lsp.enable('sourcekit')
 
-      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-        vim.lsp.handlers.hover, { border = 'rounded' })
+      -- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+      --   vim.lsp.handlers.hover, { border = 'rounded' })
 
-      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-        vim.lsp.handlers.signature_help, { border = 'rounded' })
+      -- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+      --   vim.lsp.handlers.signature_help, { border = 'rounded' })
 
       -- Global mappings.
       -- See `:help vim.diagnostic.*` for documentation on any of the below functions
